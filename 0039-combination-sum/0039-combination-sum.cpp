48a1ddf7f9c34e4e6144e8790632564568aa8907
Jiyarathore <96529109+Jiyarathore@users.noreply.github.com>
2023-03-06 19:50:49 +0530

    Time: 8 ms (59.50%), Space: 12.1 MB (43.85%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    
+    void helper(vector<vector<int>>&ans,vector<int>v, vector<int>&candidates, int target, int idx){
+        if(target==0){
+            ans.push_back(v);
+            return;
+        }
+        
+        if(target<0) {
+            return;
+        }
+        
+        //body of recursion
+        
+        for(int i=idx;i<candidates.size();i++){
+            if(candidates[i]<=target){
+                v.push_back(candidates[i]);
+                helper(ans,v,candidates,target-candidates[i],i);
+                v.pop_back();
+            }
+            else break;
+        }
+        return;
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        sort(candidates.begin(),candidates.end());
+        
+        helper(ans,v,candidates,target,0);
+        
+        return ans;
+        
+    }
+};
