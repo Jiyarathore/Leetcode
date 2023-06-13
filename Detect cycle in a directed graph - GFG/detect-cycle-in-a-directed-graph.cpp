ebba1f123a0f21972f98aeb38022f1f7f9146e01
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-13 12:12:15 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,41 +4,74 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    bool dfsCheck(int node,vector<int>adj[],int vis[],int pathvis[]){
-        vis[node]=1;
-        pathvis[node]=1;
+    // using dfs
+//     private:
+//     bool dfsCheck(int node,vector<int>adj[],int vis[],int pathvis[]){
+//         vis[node]=1;
+//         pathvis[node]=1;
         
-        // traverse for adjacent nodes
-        for(auto it: adj[node]){
-            // whenn node is not visited
-            if(!vis[it]){
-                if(dfsCheck(it,adj,vis,pathvis)==true)
-                return true;
+//         // traverse for adjacent nodes
+//         for(auto it: adj[node]){
+//             // whenn node is not visited
+//             if(!vis[it]){
+//                 if(dfsCheck(it,adj,vis,pathvis)==true)
+//                 return true;
                 
-            }
-            // if node has been previously visited 
-            // but it has to be visited on same path
-            else if(pathvis[it]){
-                return true;
-            }
-        }
-        pathvis[node]=0;
-        return false;
-    }
-  public:
-    // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        int vis[V]={0};
-        int pathvis[V]={0};
+//             }
+//             // if node has been previously visited 
+//             // but it has to be visited on same path
+//             else if(pathvis[it]){
+//                 return true;
+//             }
+//         }
+//         pathvis[node]=0;
+//         return false;
+//     }
+//   public:
+//     // Function to detect cycle in a directed graph.
+//     bool isCyclic(int V, vector<int> adj[]) {
+//         // code here
+//         int vis[V]={0};
+//         int pathvis[V]={0};
         
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 if(dfsCheck(i,adj,vis,pathvis)==true) return true;
+//             }
+//         }
+//         return false;
+//     }
+
+    // using bfs toposort kahn
+    
+    public:
+    bool isCyclic(int V, vector<int>adj[]){
+        int indegree[V]={0};
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfsCheck(i,adj,vis,pathvis)==true) return true;
+            for(auto it: adj[i]){
+                indegree[it]++;
             }
         }
-        return false;
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            c++;
+            
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        
+        if(c==V) return false;
+        return true;
     }
 };
 
