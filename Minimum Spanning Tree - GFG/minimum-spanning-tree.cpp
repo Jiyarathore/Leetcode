8edbfb6c771fceca751f597a9192d0d5b3808fd5
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-16 17:36:53 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,43 +3,130 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet{
+    vector<int>rank,parent,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int findPar(int node){
+        if(node==parent[node])
+        return node;
+        return parent[node]=findPar(parent[node]);
+    }
+    
+    void unionByRank(int u,int v){
+        int ulp_u=findPar(u);
+        int ulp_v=findPar(v);
+        
+        if(ulp_u==ulp_v) return;
+        
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulp_u=findPar(u);
+        int ulp_v=findPar(v);
+        
+         if(ulp_u==ulp_v) return;
+        
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        // code here
-        priority_queue<pair<int,int>,
-        vector<pair<int,int>>,greater<pair<int,int>>>pq;
+	//Function to find sum of weights of edges of the Minimum Spanning Tree. by Prims Algorithm
+    // int spanningTree(int V, vector<vector<int>> adj[])
+    // {
+    //     // code here
+    //     priority_queue<pair<int,int>,
+    //     vector<pair<int,int>>,greater<pair<int,int>>>pq;
         
-        vector<int>vis(V,0);
-        //      wt,node
-        pq.push({0,0});
-        int sum=0;
+    //     vector<int>vis(V,0);
+    //     //      wt,node
+    //     pq.push({0,0});
+    //     int sum=0;
         
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
+    //     while(!pq.empty()){
+    //         auto it=pq.top();
+    //         pq.pop();
             
-            int node=it.second;
-            int wt=it.first;
+    //         int node=it.second;
+    //         int wt=it.first;
             
-            if(vis[node]==1) continue;
+    //         if(vis[node]==1) continue;
             
-            vis[node]=1;
-            sum+=wt;
+    //         vis[node]=1;
+    //         sum+=wt;
             
-            for(auto it:adj[node]){
+    //         for(auto it:adj[node]){
+    //             int adjNode=it[0];
+    //             int edW=it[1];
+    //             if(!vis[adjNode]){
+    //                 pq.push({edW,adjNode});
+    //             }
+    //         }
+    //     }
+    //     return sum;
+    // }
+    
+    
+    
+    // Minimum Spanning Tree. by Kruskal’s Algorithm
+    
+    int spanningTree(int V, vector<vector<int>>adj[]){
+        
+        vector<pair<int,pair<int,int>>>edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
                 int adjNode=it[0];
-                int edW=it[1];
-                if(!vis[adjNode]){
-                    pq.push({edW,adjNode});
-                }
+                int wt=it[1];
+                int node=i;
+                
+                edges.push_back({wt,{node,adjNode}});
             }
         }
-        return sum;
+        DisjointSet ds(V);
+        sort(edges.begin(),edges.end());
+        int mstWt=0;
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(ds.findPar(u) != ds.findPar(v)){
+                mstWt+=wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        return mstWt;
     }
+    
 };
 
 //{ Driver Code Starts.
