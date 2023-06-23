68ead22cf0e2e1f06c3e8c8722a8106c028676d9
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-23 19:54:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=2) return n;
-        int x=1,y=2;
-        for(int i=3;i<n;i++){
-            int t=y;
-            y+=x;
-            x=t;
+//        Tabulation approach
+        vector<int>dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return x+y;
+        return dp[n];
     }
 };
