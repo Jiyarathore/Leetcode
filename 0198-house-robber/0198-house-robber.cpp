d2fa718dd3dd8daebaee6986d862054eb3fc2f1b
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-24 15:29:45 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (96.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,23 +5,44 @@ public:
     
 //     Tabulation Method
     
-    int solveUtil(int n, vector<int>& arr, vector<int>& dp){
-    dp[0]=arr[0];
+//     int solveUtil(int n, vector<int>& arr, vector<int>& dp){
+//     dp[0]=arr[0];
+        
+//         for(int i=1;i<n;i++){
+//             int pick=arr[i];
+//             if(i>1)
+//                 pick+=dp[i-2];
+//             int nonPick=0 + dp[i-1];
+            
+//             dp[i]=max(pick,nonPick);
+//         }
+//     return dp[n-1];
+// }
+    
+//     int rob(vector<int>& arr) {
+//         int n=arr.size();
+//         vector<int> dp(n,-1);
+//         return solveUtil(n, arr, dp);
+//     }
+
+    
+//     Space OPtimization
+    
+    int rob(vector<int>&arr){
+        int n=arr.size();
+        int prev=arr[0];
+        int prev2=0;
         
         for(int i=1;i<n;i++){
             int pick=arr[i];
             if(i>1)
-                pick+=dp[i-2];
-            int nonPick=0 + dp[i-1];
+                pick+=prev2;
+            int nonPick=0 + prev;
             
-            dp[i]=max(pick,nonPick);
+            int cur_i=max(pick,nonPick);
+            prev2=prev;
+            prev=cur_i;
         }
-    return dp[n-1];
-}
-    
-    int rob(vector<int>& arr) {
-        int n=arr.size();
-        vector<int> dp(n,-1);
-        return solveUtil(n, arr, dp);
+        return prev;
     }
 };
