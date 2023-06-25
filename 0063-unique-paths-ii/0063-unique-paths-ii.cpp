32e6f52a3077866f2c61bb7dc713380a5c791926
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-25 15:51:36 +0530

    Time: 3 ms (70.07%), Space: 7.5 MB (89.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//     Before solution
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
       int rows = obstacleGrid.size();
         int cols = obstacleGrid[0].size();
@@ -36,4 +37,58 @@ public:
         }
         return dp[rows-1][cols-1];
     }
+    
+    
+//     Memorization Method
+    
+//    int mazeObstaclesUtil(int i, int j, vector<vector<int> > &maze, vector<vector<int> > 
+// &dp) {
+//   if(i>0 && j>0 && maze[i][j]==-1) return 0; 
+//   if(i==0 && j == 0)
+//     return 1;
+//   if(i<0 || j<0)
+//     return 0;
+//   if(dp[i][j]!=-1) return dp[i][j];
+    
+//   int up = mazeObstaclesUtil(i-1,j,maze,dp);
+//   int left = mazeObstaclesUtil(i,j-1,maze,dp);
+  
+//   return dp[i][j] = up+left;
+  
+// }
+    
+//     Tabulation 
+    
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid){
+//          int n = obstacleGrid.size();
+//         int m = obstacleGrid[0].size();
+//        vector<vector<int> > dp(n,vector<int>(m,-1));
+//    for(int i=0; i<n ;i++){
+//       for(int j=0; j<m; j++){
+          
+//           //base conditions
+//           if(i>0 && j>0 && obstacleGrid[i][j]==1){
+//             dp[i][j]=0;
+//             continue;
+//           }
+//           if(i==0 && j==0){
+//               dp[i][j]=1;
+//               continue;
+//           }
+          
+//           int up=0;
+//           int left = 0;
+          
+//           if(i>0) 
+//             up = dp[i-1][j];
+//           if(j>0)
+//             left = dp[i][j-1];
+            
+//           dp[i][j] = up+left;
+//       }
+       
+//     }
+//          return dp[n-1][m-1];
+//     }
+    
 };
