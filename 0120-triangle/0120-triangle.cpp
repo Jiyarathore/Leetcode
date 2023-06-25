f86a3c3ce5daf94fdae185fcb1de786322170f13
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-25 19:18:05 +0530

    Time: 8 ms (55.72%), Space: 9.1 MB (22.17%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,20 +2,41 @@ class Solution {
 public:
     
 //     Memorixation
-    int minPath(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp){
-        if(dp[i][j]!=-1) return dp[i][j];
+//     int minPath(int i,int j,vector<vector<int>>& triangle,int n,vector<vector<int>>& dp){
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(i==n-1) return triangle[i][j];
+//         if(i==n-1) return triangle[i][j];
         
-        int dn=triangle[i][j] + minPath(i+1,j,triangle,n,dp);
-        int dg=triangle[i][j] + minPath(i+1,j+1,triangle,n,dp);
+//         int dn=triangle[i][j] + minPath(i+1,j,triangle,n,dp);
+//         int dg=triangle[i][j] + minPath(i+1,j+1,triangle,n,dp);
         
-        return dp[i][j]=min(dn,dg);
-    }
+//         return dp[i][j]=min(dn,dg);
+//     }
     
-    int minimumTotal(vector<vector<int>>& triangle) {
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n=triangle.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+//         return minPath(0,0,triangle,n,dp);
+//     }
+    
+//     Tabulation
+    
+      int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return minPath(0,0,triangle,n,dp);
-    }
-};
+        vector<vector<int>>dp(n,vector<int>(n,0));
+          
+          for(int j=0;j<n;j++){
+              dp[n-1][j]=triangle[n-1][j];
+          }
+          
+          for(int i=n-2;i>=0;i--){
+              for(int j=i;j>=0;j--){
+                  int dn=triangle[i][j] + dp[i+1][j];
+                  int dg=triangle[i][j] + dp[i+1][j+1];
+                  
+                  dp[i][j]=min(dn,dg);
+              }
+          }
+          return dp[0][0];
+      }
+      };
