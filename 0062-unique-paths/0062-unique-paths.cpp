e5d4f3d44b431883428be816d7f774ef0ace4314
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-06-25 12:44:31 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (22.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,1));
+//     easy solution
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>>dp(m,vector<int>(n,1));
         
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+//         for(int i=1;i<m;i++){
+//             for(int j=1;j<n;j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+    
+//     Memorization
+    
+    int countWays(int i,int j,vector<vector<int>> &dp){
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int up = countWays(i-1,j,dp);
+        int left =  countWays(i,j-1,dp);
+        
+        return dp[i][j] = up+left;
     }
+    
+    int uniquePaths(int m,int n){
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return countWays(m-1,n-1,dp);
+    }
+    
 };
