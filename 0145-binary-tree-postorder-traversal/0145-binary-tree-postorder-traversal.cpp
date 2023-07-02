2e3ec99bb18ead88bdceeb0a155443aa33a9031d
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-02 16:51:10 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (81.06%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -10,19 +10,47 @@
  * };
  */
 class Solution {
-    
-    void postorderfun(vector<int>&postorder,TreeNode* root){
-        if(root==NULL) return;
+//     Recursive sol
+//     void postorderfun(vector<int>&postorder,TreeNode* root){
+//         if(root==NULL) return;
         
-        postorderfun(postorder,root->left);
-        postorderfun(postorder,root->right);
-        postorder.push_back(root->val);
-    }
+//         postorderfun(postorder,root->left);
+//         postorderfun(postorder,root->right);
+//         postorder.push_back(root->val);
+//     }
     
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>postorder;
-        postorderfun(postorder,root);
-        return postorder;
+        // vector<int>postorder;
+        // postorderfun(postorder,root);
+        // return postorder;
+        
+//         Ierative sol using 2 stacks
+        
+        vector<int>postOrder;
+        if(root==NULL) return postOrder;
+        
+        stack<TreeNode*>s1;
+        stack<TreeNode*>s2;
+        
+        s1.push(root);
+        
+        while(!s1.empty()){
+            root=s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left!=NULL){
+                s1.push(root->left);
+            }
+            if(root->right!=NULL){
+                s1.push(root->right);
+            }
+        }
+        
+        while(!s2.empty()){
+            postOrder.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return postOrder;
     }
 };
