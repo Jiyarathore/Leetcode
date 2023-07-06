27bcdee6e2793776dead8943a93202d246b19032
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-06 15:41:46 +0530

    Time: 271 ms (91.65%), Space: 144.6 MB (85.81%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -14,23 +14,51 @@ public:
     
 //     using dfs
     
-    int dfs(TreeNode* root){
+//     int dfs(TreeNode* root){
+//         if(root==NULL) return 0;
+        
+// //         if only one of the child is non null, then go in recursion
+//         if(root->left==NULL){
+//             return 1+dfs(root->right);
+//         }
+        
+//         else if(root->right==NULL){
+//             return 1+ dfs(root->left);
+//         }
+        
+// //         both children are non null , call for both childs
+//         return 1+ min(dfs(root->left),dfs(root->right));
+//     }
+    
+//     int minDepth(TreeNode* root) {
+//     return dfs(root);    
+//     }
+    
+//     using bfd
+    
+    int minDepth(TreeNode* root){
         if(root==NULL) return 0;
         
-//         if only one of the child is non null, then go in recursion
-        if(root->left==NULL){
-            return 1+dfs(root->right);
-        }
+        queue<TreeNode*>q;
+        q.push(root);
+        int depth=1;
         
-        else if(root->right==NULL){
-            return 1+ dfs(root->left);
+        while(!q.empty()){
+            int qSize=q.size();
+            
+            while(qSize--){
+                TreeNode* node=q.front();
+                q.pop();
+                
+                if(node==NULL) continue;
+                
+                if(node->left==NULL && node->right==NULL) return depth;
+                
+                q.push({node->left});
+                q.push({node->right});
+            }
+            depth++;
         }
-        
-//         both children are non null , call for both childs
-        return 1+ min(dfs(root->left),dfs(root->right));
-    }
-    
-    int minDepth(TreeNode* root) {
-    return dfs(root);    
+        return -1;
     }
 };
