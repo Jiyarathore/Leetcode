7a8b17688a7bff412f71f8f0bfcdf297cbdaf315
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-06 13:33:24 +0530

    Time: 4 ms (16.91%), Space: 6 MB (76.20%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -18,21 +18,38 @@ public:
     
 //     memorixation
     
-    int numTree(int n, vector<int> &dp) {
-	// base case
-	if (n <= 1) return 1;
+//     int numTree(int n, vector<int> &dp) {
+// 	// base case
+// 	if (n <= 1) return 1;
 	
-	if (! dp[n])
+// 	if (! dp[n])
 		
-		for (int i = 1; i <= n; i++)
-			dp[n] += numTree(i - 1, dp) * numTree(n - i, dp);
+// 		for (int i = 1; i <= n; i++)
+// 			dp[n] += numTree(i - 1, dp) * numTree(n - i, dp);
 	
-	return dp[n];
-}
+// 	return dp[n];
+// }
 
-int numTrees(int n) {
+// int numTrees(int n) {
 	
-	vector<int> dp(20);
-	return numTree(n, dp);
-}
+// 	vector<int> dp(20);
+// 	return numTree(n, dp);
+// }
+    
+//     Tbu;ation
+    
+    int numTrees(int n){
+        vector<int>dp(n+1);
+        
+        dp[0]=dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1] * dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+    
+    
 };
