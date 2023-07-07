f9a1f747da980ba3dc55e05772b14f2c015293e3
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-07 13:03:05 +0530

    Time: 27 ms (24.51%), Space: 16.8 MB (78.85%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,19 +18,42 @@ public:
 
 class Solution {
 public:
-    Node* connect(Node* root) {
-        if(root==NULL) return NULL;
+    
+//     recursive method
+    
+//     Node* connect(Node* root) {
+//         if(root==NULL) return NULL;
         
-        if(root->left!=NULL){
-            root->left->next=root->right;
+//         if(root->left!=NULL){
+//             root->left->next=root->right;
+//         }
+//         if(root->right!=NULL && root->next!=NULL){
+//             root->right->next=root->next->left;
+//         }
+        
+//         connect(root->left);
+//         connect(root->right);
+        
+//         return root;
+//     }
+    
+//     Iterative mtd
+    
+     Node* connect(Node* root){
+         //Initialize pointers
+        Node *prev = root, *curr;
+        while (prev) {
+            curr = prev;
+            while (curr && curr->left) { 
+                //connects the left subtree of same level with right subtree of that same level 
+                curr->left->next = curr->right;
+                //connect the rightmost node of a level to the leftmost node of the next level.
+                if (curr -> next) curr->right->next = curr->next->left;
+                curr = curr->next;
+            }
+            prev = prev -> left;
         }
-        if(root->right!=NULL && root->next!=NULL){
-            root->right->next=root->next->left;
-        }
-        
-        connect(root->left);
-        connect(root->right);
-        
         return root;
-    }
+     }
+    
 };
