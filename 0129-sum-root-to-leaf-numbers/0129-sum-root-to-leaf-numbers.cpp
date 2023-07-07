ca748e4156dfca108d6db46eea52d2b880d70dfa
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-07 13:54:36 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (25.82%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,22 +11,46 @@
  */
 class Solution {
 public:
-    int sumNumbers(TreeNode* root) {
-        int currentSum=0;
-        return sum(root,currentSum);
-    }
+//     recursion 
+//     int sumNumbers(TreeNode* root) {
+//         int currentSum=0;
+//         return sum(root,currentSum);
+//     }
     
-    int sum(TreeNode* root, int currentSum){
-        if(root==NULL) return 0;
+//     int sum(TreeNode* root, int currentSum){
+//         if(root==NULL) return 0;
         
-        currentSum= currentSum*10 + root->val;
+//         currentSum= currentSum*10 + root->val;
         
-        if(root->left==NULL && root->right==NULL) return currentSum;
+//         if(root->left==NULL && root->right==NULL) return currentSum;
         
-        int leftSum=sum(root->left,currentSum);
-        int rightSum=sum(root->right,currentSum);
+//         int leftSum=sum(root->left,currentSum);
+//         int rightSum=sum(root->right,currentSum);
         
-        return leftSum+rightSum;
-    }
+//         return leftSum+rightSum;
+//     }
+    
+//     iterayive 
+    
+     int sumNumbers(TreeNode* root) {
+         int totalSun=0;
+         queue<pair<TreeNode*,int>>q;
+         q.push({root,0});
+             int totalSum=0;         
+         while(!q.empty()){
+
+             auto [root,currentSum]=q.front();
+             q.pop();
+             
+             currentSum=currentSum*10 + root->val;
+             
+             if(root->left==NULL && root->right==NULL) totalSum+=currentSum;
+             
+             if(root->left!=NULL) q.push({root->left,currentSum});
+             if(root->right!=NULL) q.push({root->right,currentSum});
+         }
+         return totalSum;
+     }
+    
     
 };
