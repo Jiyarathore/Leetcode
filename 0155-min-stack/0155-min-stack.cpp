5f4d937a2185d49d74d004df8b40d996598ded2d
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-09 11:01:04 +0530

    Time: 30 ms (22.12%), Space: 16.1 MB (96.70%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,80 @@
 class MinStack {
 public:
 //     TC-O(2N);
-    stack<pair<int,int>>st;
-    MinStack() {
+//     stack<pair<int,int>>st;
+//     MinStack() {
         
+//     }
+    
+//     void push(int val) {
+//         int mini;
+//         if(st.empty()){
+//             mini=val;
+//         }
+//         else{
+//             mini=min(st.top().second,val);
+//         }
+//         st.push({val,mini});
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//     }
+    
+//     TC-O(N);
+    
+     stack<long long>st;
+    long long mini;
+    MinStack() {
+        while(st.empty()==false) st.pop();
+        mini=INT_MAX;
     }
     
     void push(int val) {
-        int mini;
+        long long vl=val;
         if(st.empty()){
-            mini=val;
+            mini=vl;
+            st.push(vl);
         }
         else{
-            mini=min(st.top().second,val);
+            if(vl<mini){
+                st.push(2*vl*1LL - mini);
+                mini=vl;
+            }
+            else{
+                st.push(vl);
+            }
         }
-        st.push({val,mini});
     }
     
     void pop() {
+        if(st.empty()) return;
+        long long el=st.top();
         st.pop();
+        
+        if(el<mini){
+            mini=2*mini - el;
+        }
     }
     
     int top() {
-        return st.top().first;
+        if(st.empty()) return -1;
+        
+        long long el=st.top();
+        if(el<mini) return mini;
+        return el;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
 
