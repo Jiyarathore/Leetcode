71b687c5a44ce9ea279a211fe75d2b1d4e20adf5
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-10 17:37:03 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,41 +6,72 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
+    // BFS
     
-    bool detect(int src, vector<int>adj[],int vis[]){
-        vis[src]=1;
+    // bool detect(int src, vector<int>adj[],int vis[]){
+    //     vis[src]=1;
         
-        queue<pair<int,int>>q;
-        q.push({src,-1});
+    //     queue<pair<int,int>>q;
+    //     q.push({src,-1});
         
-        while(!q.empty()){
-            int node=q.front().first;
-            int parent=q.front().second;
+    //     while(!q.empty()){
+    //         int node=q.front().first;
+    //         int parent=q.front().second;
             
-            q.pop();
+    //         q.pop();
             
-            for(auto adjacentnode:adj[node]){
-                if(vis[adjacentnode]==0){
-                    vis[adjacentnode]=1;
-                    q.push({adjacentnode,node});
-                    }
-                    else if(parent!=adjacentnode) return true;
+    //         for(auto adjacentnode:adj[node]){
+    //             if(vis[adjacentnode]==0){
+    //                 vis[adjacentnode]=1;
+    //                 q.push({adjacentnode,node});
+    //                 }
+    //                 else if(parent!=adjacentnode) return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    // bool isCycle(int V, vector<int> adj[]) {
+    //     // Code here
+    //   int vis[V]={0};
+        
+    //     for(int i=0;i<V;i++){
+    //         if(!vis[i]){
+    //             if(detect(i,adj,vis)==true) return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    
+    
+    // DFS
+    
+    bool dfs(int node, int parent, int vis[], vector<int> adj[]) {
+        vis[node] = 1; 
+        // visit adjacent nodes
+        for(auto adjacentNode: adj[node]) {
+            // unvisited adjacent node
+            if(!vis[adjacentNode]) {
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
             }
+            // visited node but not a parent node
+            else if(adjacentNode != parent) return true; 
         }
-        return false;
+        return false; 
     }
     
-    bool isCycle(int V, vector<int> adj[]) {
-        // Code here
-       int vis[V]={0};
-        
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(detect(i,adj,vis)==true) return true;
-            }
-        }
-        return false;
+    bool isCycle(int V, vector<int> adj[]){
+         int vis[V] = {0}; 
+       // for graph with connected components 
+       for(int i = 0;i<V;i++) {
+           if(!vis[i]) {
+               if(dfs(i, -1, vis, adj) == true) return true; 
+           }
+       }
+       return false; 
     }
+    
 };
 
 //{ Driver Code Starts.
