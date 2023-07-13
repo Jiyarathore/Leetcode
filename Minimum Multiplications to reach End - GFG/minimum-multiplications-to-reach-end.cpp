667a9f7a0e7aaf9405d0fee80f9e48ee6902dae1
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-07-13 14:57:35 +0530

    Added solution - LeetHub

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
@@ -12,29 +12,43 @@ class Solution {
   public:
     int minimumMultiplications(vector<int>& arr, int start, int end) {
         // code here
-        queue<pair<int,int>>q;
-        q.push({start,0});
-        vector<int>dist(100000,1e9);
-        dist[start]=0;
-        int mod=100000;
-        
-        while(!q.empty()){
-            int node= q.front().first;
-            int steps=q.front().second;
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+
+        // Create a dist array to store the no. of multiplications to reach
+        // a particular number from the start number.
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        int mod = 100000;
+
+        // Multiply the start no. with each of numbers in the arr
+        // until we get the end no.
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int steps = q.front().second;
             q.pop();
-            
-            for(auto it:arr){
-                int num=(it*node)%mod;
-                
-                if(steps+1 < dist[num]){
-                    dist[num]=steps+1;
-                    
-                    if(num==end) return steps+1;
-                    q.push({num,steps+1});
+
+            for (auto it : arr)
+            {
+                int num = (it * node) % mod;
+
+                // If the no. of multiplications are less than before
+                // in order to reach a number, we update the dist array.
+                if (steps + 1 < dist[num])
+                {
+                    dist[num] = steps + 1;
+
+                    // Whenever we reach the end number
+                    // return the calculated steps
+                    if (num == end)
+                        return steps + 1;
+                    q.push({num, steps + 1});
                 }
             }
         }
-        return -1;
+        // If the end no. is unattainable.
+        return dist[end]==1e9?-1:dist[end];
     }
 };
 
