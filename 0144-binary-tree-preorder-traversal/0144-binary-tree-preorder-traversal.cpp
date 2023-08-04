c601bbdc664df0834d55cad0fce5a082782ba0bc
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-08-04 11:43:14 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (59.12%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -19,29 +19,62 @@ public:
     //     preorderfun(preorder,root->right);
     // }
     
-    vector<int> preorderTraversal(TreeNode* root) {
-        // vector<int>preorder;
-        // preorderfun(preorder,root);
-        // return preorder;
+//     Iteratibve 
+    
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         // vector<int>preorder;
+//         // preorderfun(preorder,root);
+//         // return preorder;
         
+//         vector<int>preorder;
+//     if(root==NULL) return preorder;
+
+//     stack<TreeNode*>st;
+//     st.push(root);
+//     while(!st.empty()){
+//         root = st.top();
+//         st.pop();
+//         preorder.push_back(root->val);
+
+//         if(root->right!=NULL){
+//             st.push(root->right);
+//         }
+
+//         if(root->left!=NULL){
+//             st.push(root->left);
+//         }
+//     }
+//     return preorder;
+//     }
+    
+    //     Morris Traversal treading wlaa
+    vector<int> preorderTraversal(TreeNode* root){
         vector<int>preorder;
-    if(root==NULL) return preorder;
-
-    stack<TreeNode*>st;
-    st.push(root);
-    while(!st.empty()){
-        root = st.top();
-        st.pop();
-        preorder.push_back(root->val);
-
-        if(root->right!=NULL){
-            st.push(root->right);
+        TreeNode *cur=root;
+        
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                preorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    preorder.push_back(cur->val);
+                    cur=cur->left;
+                }
+                else{
+                    prev->right=NULL;
+                    cur=cur->right;
+                }
+            }
         }
-
-        if(root->left!=NULL){
-            st.push(root->left);
-        }
-    }
-    return preorder;
+        return preorder;
     }
 };
