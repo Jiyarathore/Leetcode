913bf66ea3aaa52bec848f1ee59706074fd99b34
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-08-12 19:00:58 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (88.64%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -4,22 +4,45 @@ public:
 //     Time Complexity O(logn)
 //     Space Complexity O(logn) hash table unordered set space
     
+//     int nextNumber(int n){
+//         int newNumber=0;
+//         while(n!=0){
+// int num=n%10;
+//             newNumber+=num*num;
+//             n=n/10;
+//         }
+//         return newNumber;
+//     }
+    
+//     bool isHappy(int n) {
+//         unordered_set<int>set;
+//         while(n!=1 && set.count(n)==0){
+//             set.insert(n);
+//             n=nextNumber(n);
+//         }
+//         return n==1;
+//     }
+    
+//     Floyd's Cycle Finding ALgo/ Hare-Tortoise algorithm
+    
     int nextNumber(int n){
         int newNumber=0;
         while(n!=0){
-int num=n%10;
-            newNumber+=num*num;
+            int num=n%10;
+            newNumber += num*num;
             n=n/10;
         }
         return newNumber;
     }
     
-    bool isHappy(int n) {
-        unordered_set<int>set;
-        while(n!=1 && set.count(n)==0){
-            set.insert(n);
-            n=nextNumber(n);
+    bool isHappy(int n){
+        int slowPointer=n;
+        int fastPointer=nextNumber(n);
+        
+        while(fastPointer!=1 && fastPointer != slowPointer){
+            slowPointer=nextNumber(slowPointer);
+            fastPointer=nextNumber(nextNumber(fastPointer));
         }
-        return n==1;
+        return fastPointer==1;
     }
 };
