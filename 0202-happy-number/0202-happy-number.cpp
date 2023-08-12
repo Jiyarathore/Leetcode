558888726fe727365008bef8bce690471f9228dd
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-08-12 18:55:49 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.01%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+//     Time Complexity O(logn)
+//     Space Complexity O(logn) hash table unordered set space
+    
+    int nextNumber(int n){
+        int newNumber=0;
+        while(n!=0){
+int num=n%10;
+            newNumber+=num*num;
+            n=n/10;
+        }
+        return newNumber;
+    }
+    
+    bool isHappy(int n) {
+        unordered_set<int>set;
+        while(n!=1 && set.count(n)==0){
+            set.insert(n);
+            n=nextNumber(n);
+        }
+        return n==1;
+    }
+};
