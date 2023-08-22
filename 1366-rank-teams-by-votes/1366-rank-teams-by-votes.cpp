4feb3cb5c9dde3e8319514e93fece79bd36c8bdb
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-08-22 16:38:25 +0530

    Time: 12 ms (75.10%), Space: 10.6 MB (86.69%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+     string rankTeams(vector<string>& votes) {
+    	int n = votes.size(), m = votes[0].size();
+    	string ans = "";
+    	vector<vector<int>> v(26, vector<int>(m + 1, 0));
+    	for (auto i = 0; i < 26; i++) {
+    		v[i][m] = -(int('A') + i);
+    	}
+    	for (auto i = 0; i < n; i++) {
+    		for (auto j = 0; j < m; j++) {
+    			v[votes[i][j] - 'A'][j]++;
+    		}
+    	}
+    	sort(v.begin(),v.end(),greater<>());
+    	for (auto i = 0; i < m; i++) {
+    		ans += char(-v[i][m]);
+    	}
+    	return ans;
+    }
+};
