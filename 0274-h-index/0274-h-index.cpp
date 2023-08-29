dd14781e405d8d653b6d2d9449945fbc722d46f1
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-08-29 16:20:21 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (58.39%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+//         TC O(nlogn)
+        sort(citations.begin(), citations.end());
+        
+        int h=citations.size();
+        for(int i=0;i<citations.size();i++){
+            if(citations[i]>=h) return h;
+            
+            else{
+                if(h==1 && citations[i]!=0) return 1;
+                
+                h--;
+            }
+        }
+        return h;
+    }
+};
