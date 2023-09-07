c14787e3dfbc83b32c6c9428c62455b62991fc23
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-09-07 14:30:20 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -9,30 +9,56 @@ class Solution
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     
-    int knapsackUtil(int wt[], int val[], int ind, int W, vector<vector<int>>& dp){
-        if(ind == 0){
-        if(wt[0] <=W) return val[0];
-        else return 0;
-    }
+    // Memorization
     
-    if(dp[ind][W]!=-1)
-        return dp[ind][W];
+    // int knapsackUtil(int wt[], int val[], int ind, int W, vector<vector<int>>& dp){
+    //     if(ind == 0){
+    //     if(wt[0] <=W) return val[0];
+    //     else return 0;
+    // }
+    
+    // if(dp[ind][W]!=-1)
+    //     return dp[ind][W];
         
-    int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
+    // int notTaken = 0 + knapsackUtil(wt,val,ind-1,W,dp);
     
-    int taken = INT_MIN;
-    if(wt[ind] <= W)
-        taken = val[ind] + knapsackUtil(wt,val,ind-1,W-wt[ind],dp);
+    // int taken = INT_MIN;
+    // if(wt[ind] <= W)
+    //     taken = val[ind] + knapsackUtil(wt,val,ind-1,W-wt[ind],dp);
         
-    return dp[ind][W] = max(notTaken,taken);
+    // return dp[ind][W] = max(notTaken,taken);
+    // }
+    
+    // int knapSack(int W, int wt[], int val[], int n) 
+    // { 
+    //   // Your code here
+    //   vector<vector<int>> dp(n,vector<int>(W+1,-1));
+    //   return knapsackUtil(wt, val,n-1,W,dp);
+    // }
+    
+    // Tabulation
+    
+    int knapSack(int W, int wt[], int val[], int n) {
+        vector<vector<int>>dp(n,vector<int>(W+1,0));
+        
+        for(int i=wt[0];i<=W;i++){
+            dp[0][i]=val[0];
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int cap=0;cap<=W;cap++){
+                int notTaken=0+dp[ind-1][cap];
+                int taken=INT_MIN;
+                
+                if(wt[ind]<=cap)
+                taken=val[ind]+ dp[ind-1][cap-wt[ind]];
+                
+                dp[ind][cap]=max(notTaken,taken);
+            }
+        }
+        return dp[n-1][W];
     }
     
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-       // Your code here
-       vector<vector<int>> dp(n,vector<int>(W+1,-1));
-       return knapsackUtil(wt, val,n-1,W,dp);
-    }
 };
 
 //{ Driver Code Starts.
