6e8fd94e21ecc35c2347bb9277ae46aeb0fcaa85
Jiya Rathore <96529109+Jiyarathore@users.noreply.github.com>
2023-10-06 20:23:52 +0530

    Time: 25 ms (78.91%), Space: 6.6 MB (88.00%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -33,27 +33,49 @@ public:
 //         return countUtil(s, t, ls-1, lt-1,dp);
 //     }
     
+//     TabulTion Time Complexity: O(N*M)
+// Space Complexity: O(N*M)
+    
+//      int numDistinct(string s, string t) {
+//         int n=s.size(), m=t.size();
+//          vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         
+//          for(int i=0;i<=n;i++){
+//              dp[i][0]=1;
+//          }
+//          for(int i=1;i<=m;i++){
+//              dp[0][i]=0;
+//          }
+         
+//          for(int i=1;i<=n;i++){
+//              for(int j=1;j<=m;j++){
+//                  if(s[i-1]==t[j-1]){
+//                      dp[i][j]=(dp[i-1][j-1] + dp[i-1][j])%prime;
+//                  }
+//                  else{
+//                      dp[i][j]=dp[i-1][j];
+//                  }
+//              }
+//          }
+//          return dp[n][m];
+//      }
+    
+//     Space optimization Time Complexity: O(N*M)
+// Space Complexity: O(M)
+    
      int numDistinct(string s, string t) {
         int n=s.size(), m=t.size();
-         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+         vector<int>prev(m+1,0);
          
-         for(int i=0;i<=n;i++){
-             dp[i][0]=1;
-         }
-         for(int i=1;i<=m;i++){
-             dp[0][i]=0;
-         }
+         prev[0]=1;
          
          for(int i=1;i<=n;i++){
-             for(int j=1;j<=m;j++){
+             for(int j=m;j>=1;j--){
                  if(s[i-1]==t[j-1]){
-                     dp[i][j]=(dp[i-1][j-1] + dp[i-1][j])%prime;
-                 }
-                 else{
-                     dp[i][j]=dp[i-1][j];
+                     prev[j]=(prev[j-1]+prev[j])%prime;
                  }
              }
          }
-         return dp[n][m];
+         return prev[m];
      }
 };
